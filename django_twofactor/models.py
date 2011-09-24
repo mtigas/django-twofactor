41104b825f716c143b57cb60727740f057ef5fc4
Mike Tigas <mike@tig.as>
2011-09-23 17:08:41 -0700

    TODO/WIP note to myself

--- django_twofactor/models.py
+++ django_twofactor/models.py
@@ -5,4 +5,9 @@ class UserAuthToken(models.Model):
     user = models.OneToOneField("auth.User")
     encrypted_seed = models.CharField(max_length=120) # enough for 16-byte salt + 40-byte seed
     
-    
+    # TODO:
+    # In a perfect world, the seed would be encrypted with the user's password
+    # and the `encrypted_seed` value would update whenever the user changes his/her
+    # password. This way, the seed stays entirely secure until the first factor (username+password)
+    # has been validated.
+    #  -> Is there any way to do this without going all hack-and-slash in here?
