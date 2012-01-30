140246473dafb44e7841ab9dd77070055c9d9321
Mike Tigas <mike@tig.as>
2012-01-30 12:37:26 -0800

    very basic case for a front-end login form

--- twofactor_demo/urls.py
+++ twofactor_demo/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django_twofactor.auth_forms import TwoFactorAuthenticationForm
 
 # Replace `admin.site` with `twofactor_admin_site` before doing autodiscover
 # so that we can get the default auto-registered behavior BUT use our
@@ -9,12 +10,13 @@ admin.site = twofactor_admin_site
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'twofactor_demo.views.home', name='home'),
-    # url(r'^twofactor_demo/', include('twofactor_demo.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+
+    (r'^$', 'django.contrib.auth.views.login', {
+        'template_name': 'login.html',
+        'authentication_form': TwoFactorAuthenticationForm
+    }),
+    (r'^logout/$', 'django.contrib.auth.views.logout_then_login', {
+        'login_url': '/'
+    }),
 )
